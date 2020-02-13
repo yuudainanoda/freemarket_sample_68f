148c1f5356a5c5a23c9156a299d6c8b5e84b3a25
atsushi-tago-0209 <0209at@gmail.com>
2020-02-13 20:07:27 +0900

    rails g devise:views 実行後　コミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_023027) do
+ActiveRecord::Schema.define(version: 2020_02_13_101710) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
