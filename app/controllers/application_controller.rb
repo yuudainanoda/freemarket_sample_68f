f45d8a343004eb9b4a4872f2e5b33074f863d53d
atsushi-tago-0209 <0209at@gmail.com>
2020-02-16 17:21:36 +0900

    バリデーション追記　挙動確認未
    マイグレーションファイルに書いたらいいのか
    user.rbに書いたらいいのか迷っています。
    記述に誤りがあれば指摘いただければと思います。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: devise_controller?
 
   private
 
@@ -11,5 +12,13 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def 
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password,:first_name,:first_name_kana,:last_name,:last_name_kana,:birth_year,:birth_month,:birth_day,:telephone])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_121007) do
+ActiveRecord::Schema.define(version: 2020_02_16_025105) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
