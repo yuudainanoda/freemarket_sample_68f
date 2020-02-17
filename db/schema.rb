3ff449a7d43e36e7c6bd32a246819621c688567e
yuudainanoda <kashibayuudai@icloud.com>
2020-02-17 19:50:54 +0900

    items_edit_kitada

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_121007) do
+ActiveRecord::Schema.define(version: 2020_02_17_100149) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
