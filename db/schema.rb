b8def76103fdde7bb01546e85ef8443e502fc417
atsushi-tago-0209 <0209at@gmail.com>
2020-02-18 14:53:55 +0900

    まだ作成前　重要でない記録

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_02_16_085520) do
-
+ActiveRecord::Schema.define(version: 2020_02_17_100149) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
