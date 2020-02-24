bbd9040ac7ba0b6b2ed8d8eaeed7946644e8e5d6
atsushi-tago-0209 <0209at@gmail.com>
2020-02-24 16:47:03 +0900

    ログイン状態にできる

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,105 +13,102 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user.build_street_address
   end
 
-  def create
-    # binding.pry
-    @user = User.new(sign_up_params)
-    unless @user.valid?
-      flash.now[:alert] = @user.errors.full_messages
-      render :new and return
-    end
-    if  @user.save
-
-       @user
-    else
-    redirect_to new_path
-    end
-    # session ["devise.regist_data"] = {user:@user.attributes}
-    # session ["devise.regist_data"] [:user]["password"]= params[:user][:password]
-    # @telephone = @user.build_telephone
-    # render = :new_telephone
-  end
-
-  # def create_telephone
-  #   @user = User.new(session["devise.regist_data"]["user"])
-  #   @telephone = Telephone.new(telephone_params)
-  #   unless @telephone.valid?
-  #     flash.now[:alert] = @telephone.errors.full_messages
-  #     render :new_telephone and return
-  #   end
-  #   @user.build_telephone(@telephone.attributes)
-  #   @user.save
-  #   sign_in(:user, @user)
-  # end
-
-
-  # POST /resource
   # def create
-  #   super
+  #   @user = User.new(sign_up_params)
+  #   unless @user.valid?
+  #     flash.now[:alert] = @user.errors.full_messages
+  #     render :new and return
+  #   end
+  #   if  @user.save
+  #     session[:user_id] = @user.id
+  #     redirect_to root_path
+  #   else
+  #   redirect_to new_path
+  #   end
+  #   # session ["devise.regist_data"] = {user:@user.attributes}
+  #   # session ["devise.regist_data"] [:user]["password"]= params[:user][:password]
+  #   # @telephone = @user.build_telephone
+  #   # render = :new_telephone
   # end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  # # def create_telephone
+  # #   @user = User.new(session["devise.regist_data"]["user"])
+  # #   @telephone = Telephone.new(telephone_params)
+  # #   unless @telephone.valid?
+  # #     flash.now[:alert] = @telephone.errors.full_messages
+  # #     render :new_telephone and return
+  # #   end
+  # #   @user.build_telephone(@telephone.attributes)
+  # #   @user.save
+  # #   sign_in(:user, @user)
+  # # end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  # # POST /resource
+  # # def create
+  # #   super
+  # # end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  # # GET /resource/edit
+  # # def edit
+  # #   super
+  # # end
 
-  # protected
+  # # PUT /resource
+  # # def update
+  # #   super
+  # # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  # # DELETE /resource
+  # # def destroy
+  # #   super
+  # # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  # # GET /resource/cancel
+  # # Forces the session data which is usually expired after sign
+  # # in to be expired now. This is useful if the user wants to
+  # # cancel oauth signing in/up in the middle of the process,
+  # # removing all OAuth session data.
+  # # def cancel
+  # #   super
+  # # end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  # # protected
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
+  # # If you have extra params to permit, append them to the sanitizer.
+  # # def configure_sign_up_params
+  # #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # # end
+
+  # # If you have extra params to permit, append them to the sanitizer.
+  # # def configure_account_update_params
+  # #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # # end
+
+  # # The path used after sign up.
+  # # def after_sign_up_path_for(resource)
+  # #   super(resource)
+  # # end
+
+  # # The path used after sign up for inactive accounts.
+  # # def after_inactive_sign_up_path_for(resource)
+  # #   super(resource)
+  # # end
+  # private
+  # def sign_up_params
+  #   params.require(:user).permit(
+  #     :nickname,
+  #     :email,
+  #     :first_name,
+  #     :first_name_kana,
+  #     :last_name,
+  #     :last_name_kana,
+  #     :birth_year,
+  #     :birth_month,
+  #     :birth_day,
+  #     street_address_attributes: [:id, :zipcode, :city, :address,:prefecture,:first_name,:first_name_kana,:last_name,:last_name_kana,:telephone,:_destroy]
+  #   )
   # end
-  private
-  def sign_up_params
-    params.require(:user).permit(
-      :nickname,
-      :email,
-      :password,
-      :password_confirmation,
-      :first_name,
-      :first_name_kana,
-      :last_name,
-      :last_name_kana,
-      :birth_year,
-      :birth_month,
-      :birth_day,
-      street_address_attributes: [:id, :zipcode, :city, :address,:prefecture,:first_name,:first_name_kana,:last_name,:last_name_kana,:building,:telephone,:_destroy]
-    )
-  end
 
   # protected
   # def configure_sign_up_params
--- db/schema.rb
+++ db/schema.rb
@@ -21,23 +21,6 @@ ActiveRecord::Schema.define(version: 2020_02_20_083853) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.text "description", null: false
-    t.string "brand"
-    t.string "category", null: false
-    t.string "condition", null: false
-    t.string "deriver_charge", null: false
-    t.string "area", null: false
-    t.string "deriver_date", null: false
-
-ActiveRecord::Schema.define(version: 2020_02_19_102659) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.text "image"
-    t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -45,9 +28,6 @@ ActiveRecord::Schema.define(version: 2020_02_19_102659) do
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "text"
-    t.integer "user_id"
-    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
