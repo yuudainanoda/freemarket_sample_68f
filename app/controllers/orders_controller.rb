ada39a4a96075ff70f5744a51e5e26b7b2b6c9c4
yuudainanoda <kashibayuudai@icloud.com>
2020-02-28 16:04:14 +0900

    変えたのでよろしくお願いします。　「許してください」

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,17 +9,20 @@ class OrdersController < ApplicationController
   end
 
   def pay
-    card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-    Payjp::Charge.create(
-    :amount => 1000, #支払金額を入力（itemテーブル等に紐づけても良い）
-    :customer => card.order_id, #顧客ID
-    :currency => 'jpy', #日本円
-    
-  )
-    @order = Order.new(item_id: params[:item_id], user_id: current_user.id)   
-    @order.save
-    redirect_to root_path
+    - if card = Card.where(user_id: current_user.id).first
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp::Charge.create(
+      amount: 1000, #支払金額を入力（itemテーブル等に紐づけても良い）
+      customer: card.order_id, #顧客ID
+      currency: 'jpy', #日本円
+      
+    )
+      @order = Order.new(item_id: params[:item_id], user_id: current_user.id)   
+      @order.save
+      redirect_to root_path
+  else 
+    redirect_to item_path
+  end
   end
 
   private
