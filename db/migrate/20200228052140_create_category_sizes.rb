9b1bdec8de5f36836c3bafb8f784ac4becc5e24f
KenjiBamba <horse10place26bb@yahoo.co.jp>
2020-03-02 14:37:36 +0900

    いらないモデルを削除しました

--- db/migrate/20200228052140_create_category_sizes.rb
+++
@@ -1,8 +0,0 @@
-class CreateCategorySizes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :category_sizes do |t|
-      t.references  :category
-      t.references  :size
-    end
-  end
-end
